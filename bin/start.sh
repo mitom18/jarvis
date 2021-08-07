9e914be0cd05f3330ea5ca1d8afa0eb983c2c3cd
mitom18 <michal.toman99@email.cz>
2021-08-07 19:22:28 +0200

    Bash deploy scripts update

--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,8 @@ then
   pid=`cat temp`
   echo "Process already exists; $pid"
 else
-  script='../jarvis.py'
+  file='../jarvis.py'
+  script="$(cd $(dirname \"$file\"); pwd)/$(basename $file)"
   echo "starting $script with nohup"
   nohup python3 $script &
   echo $! > temp
